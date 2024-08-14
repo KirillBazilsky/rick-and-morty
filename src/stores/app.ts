f09071f505564bb119318e5e1506888ef128f7f0
KirillBazilsky <bazilsky.fortech@gmail.com>
2024-08-14 18:10:01 +0300

    add store

--- src/stores/app.ts
+++ src/stores/app.ts
@@ -1,8 +1,19 @@
-// Utilities
-import { defineStore } from "pinia";
 
-export const useAppStore = defineStore("app", {
+import { defineStore } from 'pinia'
+
+export const useAppStore = defineStore('app', {
   state: () => ({
-    //
+    characterInfo:{} 
   }),
-});
+  getters:{
+    getCaracterInfo:(state) => state.characterInfo
+  },
+  actions: {
+    setCharacterInfo(character: Record<string, any> | null) {
+      this.characterInfo = character || {}; 
+    }
+  }
+  
+})
+
+
