ab0d7fe630551a016e81b272be1d0e2153ad4f7f
KirillBazilsky <bazilsky.fortech@gmail.com>
2024-08-16 13:13:50 +0300

    feat setup global store

--- src/stores/app.ts
+++ src/stores/app.ts
@@ -1,15 +1,51 @@
 import { defineStore } from "pinia";
+import CharactersApi from "@/api/charactersApi";
+
 
 export const useAppStore = defineStore("app", {
   state: () => ({
-    characterInfo: {},
+    isToggleMenu:<boolean>false
   }),
-  getters: {
-    getCaracterInfo: (state) => state.characterInfo,
-  },
+  
   actions: {
-    setCharacterInfo(character: Record<string, any> | null) {
-      this.characterInfo = character || {};
+    toggleMenu() {
+      this.isToggleMenu = !this.isToggleMenu; 
     },
+  },
+});
+
+export const useCharactersStore = defineStore("characters", {
+  state: () => ({
+    name:"",
+    characters: <Character[]>[],
+    species:<string | undefined>"",
+    status: <string | undefined>"",
+    gender:<string | undefined>"",
+    errorMessage: <string | null>null,
+    isLoading: <boolean>true,
+  }),
+  
+  actions: {
+    async fetchCharacters() {
+      try {
+          this.isLoading = true;
+          this.characters = await CharactersApi.getItems(
+          this.name,
+          this.species,
+          this.gender,
+          this.status,
+        );
+        
+        this.errorMessage = "";
+      } catch (error: any) {
+        if (error.response && error.response.status === 404) {
+          this.errorMessage = "No characters found for the selected filters.";
+        } else {
+          this.errorMessage = "An error occurred. Please try again later.";
+        }
+      } finally {
+        setTimeout(() => (this.isLoading = false), 300);
+      }
+    }
   },
 });
