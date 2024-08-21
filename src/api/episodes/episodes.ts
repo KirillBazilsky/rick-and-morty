e2a474adbc734292f1a1de20111be8a9c81fa3d4
KirillBazilsky <bazilsky.fortech@gmail.com>
2024-08-21 11:11:12 +0300

    feat: add Api

--- src/api/episodes/episodes.ts
+++ src/api/episodes/episodes.ts
@@ -2,7 +2,21 @@ import axios from "axios";
 import { API_URL} from "@/constatnts/constants"; 
 
 class EpisodesApi {
-  
+  public static async fetchEpisodes(episode?: string,page?: number) {
+    
+    const response = await axios.get(
+      `${API_URL}episode`,{params:{
+        episode,
+        page
+        }
+      }
+    )
+    const results = response.data.results; 
+    const info = response.data.info; 
+    
+    return { results, info }
+  }
+
   public static async getEpisodes(
     url?: string[],
     
@@ -16,6 +30,18 @@ class EpisodesApi {
     
     return response.data
   }
+
+  public static async getEpisodeInfo(
+    id?: string, 
+  ) {
+    
+    const response = await axios.get(
+      `${API_URL }episode/${id}`
+    );
+
+    return response.data;
+  }
+
 
   
  
