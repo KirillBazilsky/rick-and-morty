c92e4ae83cbe2a329d34250100fc43512b272f65
KirillBazilsky <bazilsky.fortech@gmail.com>
2024-08-21 14:58:02 +0300

    Feat: add location-details route

--- src/typed-router.d.ts
+++ src/typed-router.d.ts
@@ -20,5 +20,6 @@ declare module 'vue-router/auto-routes' {
   export interface RouteNamedMap {
     '/CharacterDetails': RouteRecordInfo<'/CharacterDetails', '/CharacterDetails', Record<never, never>, Record<never, never>>,
     '/Characters': RouteRecordInfo<'/Characters', '/Characters', Record<never, never>, Record<never, never>>,
+    '/LocationDetails': RouteRecordInfo<'/LocationDetails', '/LocationDetails', Record<never, never>, Record<never, never>>,
   }
 }
